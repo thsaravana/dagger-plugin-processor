9ada2d504c8ec70a80fd3e87ac77b4d474715463
thsaravana <th.saravana@gmail.com>
2020-05-16 17:27:16 +0530

    Creating a simple Test source file in Java

--- src/main/kotlin/com/madrapps/daggerprocessor/DaggerProcessor.kt
+++ src/main/kotlin/com/madrapps/daggerprocessor/DaggerProcessor.kt
@@ -1,12 +1,57 @@
 package com.madrapps.daggerprocessor
 
-import javax.annotation.processing.AbstractProcessor
-import javax.annotation.processing.RoundEnvironment
+import javax.annotation.processing.*
+import javax.lang.model.SourceVersion
+import javax.lang.model.element.ElementKind
 import javax.lang.model.element.TypeElement
+import javax.lang.model.util.Elements
+import javax.tools.Diagnostic
 
-class DaggerProcessor: AbstractProcessor() {
 
-    override fun process(annotations: MutableSet<out TypeElement>?, roundEnv: RoundEnvironment?): Boolean {
-        return false
+class DaggerProcessor : AbstractProcessor() {
+
+    lateinit var filer: Filer
+    lateinit var elementUtils: Elements
+    lateinit var messager: Messager
+
+    override fun init(processingEnv: ProcessingEnvironment) {
+        super.init(processingEnv)
+        filer = processingEnv.filer
+        elementUtils = processingEnv.elementUtils
+        messager = processingEnv.messager
+    }
+
+    override fun process(annotations: MutableSet<out TypeElement>, roundEnv: RoundEnvironment): Boolean {
+        roundEnv.getElementsAnnotatedWith(GenerateTest::class.java).forEach { element ->
+            messager.printMessage(Diagnostic.Kind.WARNING, "Processing $element")
+            if (element.kind == ElementKind.CLASS) {
+                val jfo = filer.createSourceFile("TestSample")
+                val writer = jfo.openWriter()
+
+                writer.use {
+                    it.write("""
+                        import org.junit.Test;
+                        
+                        public class TestSample {
+                        
+                            @Test
+                            public void testSomething() {
+                                assert(true);
+                            }
+                        }
+                    """.trimIndent())
+                }
+
+            }
+        }
+        return true
+    }
+
+    override fun getSupportedAnnotationTypes(): MutableSet<String> {
+        return mutableSetOf(GenerateTest::class.java.canonicalName)
+    }
+
+    override fun getSupportedSourceVersion(): SourceVersion {
+        return SourceVersion.latestSupported()
     }
 }
